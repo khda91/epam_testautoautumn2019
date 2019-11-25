adf8c8ca6a847754519bd59083de4e99bcb9c690
Dmitry Khodakovsky <dmitrii.khodakovsky@clarivate.com>
2019-11-25 20:54:37 +0300

    fix

--- src/test/java/lesson3/LoginSeleniumTest.java
+++ src/test/java/lesson3/LoginSeleniumTest.java
@@ -2,6 +2,7 @@ package lesson3;
 
 import lesson3.inheretence.ContactFormPage;
 import lesson3.inheretence.HomePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
@@ -59,5 +60,34 @@ public class LoginSeleniumTest extends AbstractBaseTest {
 
         assertEquals(contactFormPage.getNameTextFieldValue(), "some name");
         assertTrue(contactFormPage.isPassportCheckBoxSelected());
+    }
+
+    @Test
+    public void ttt() {
+        HomePage homePage = new HomePage(driver);
+        homePage.open();
+        homePage.login("epam", "1234");
+        homePage.headerMenuItemClick("Service");
+
+//        driver.findElement(By.xpath("//li[contains(@class, 'menu-title')]"))
+
+        driver.findElement(By.xpath("//div[@name='navigation-sidebar']//li[8]//a[1]")).click();
+
+        driver.findElement(By.xpath("//div[@class='main-content']//div[2]//label[1]")).click();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("//label[conatins(text(),'Water')] -> " + driver.findElement(By.xpath("//div[@class='main-content']//div[2]//label[1]")).isSelected());
+        System.out.println("//label[text()='Water']/input -> " + driver.findElement(By.xpath("//div[@class='main-content']//div[2]//label[1]/input")).isSelected());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
